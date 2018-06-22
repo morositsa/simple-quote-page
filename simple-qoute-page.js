23320498e064f806da58cbfafd3e72efdfc10317
Morosits Attila <morositsa@freemail.hu>
2018-06-22 12:37:49 +0200

    fordítás, átméretezés

---
+++ simple-qoute-page.js
@@ -0,0 +1,11 @@
+function magyar (){
+  document.getElementById('quoteid').innerHTML = 'Az emberiség szabadsága nem abban a tényben rejlik, hogy meg tudjuk csinálni, amit akarunk, de nem kell azt megtenni, amit nem akarunk.';
+}
+
+function angol (){
+  document.getElementById('quoteid').innerHTML = 'The freedom of Mankind does not lie in the fact that we can do what we want, but that we do not have to do that which we do not want.';
+}
+/*
+document.getElementById("demo").onclick = function() {magyar()};
+document.getElementById("demo").onclick = function() {magyar()};
+*/
